bc097fc8ce4b81d6f60e5879c2b50c829d5e3ad1
haoshuai999 <626593718@qq.com>
2020-11-30 00:38:33 -0500

    add delete and toggle functions

--- 03-adding-dynamic-behavior/src/App.svelte
+++ 03-adding-dynamic-behavior/src/App.svelte
@@ -1,5 +1,11 @@
 <script>
   import Todos from './components/Todos.svelte'
+
+  let todos = [
+    { id: 1, name: 'Eat', completed: true },
+    { id: 2, name: 'Swim', completed: true },
+    { id: 3, name: 'Run', completed: false }
+  ]
 </script>
 
-<Todos />
+<Todos {todos}/>
